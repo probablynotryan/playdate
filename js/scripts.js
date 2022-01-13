cea9984c9859ebfea59b115ef4ec54ae334d4dc7
Ryan Bass <theryanbass@gmail.com>
2022-01-13 15:14:32 -0800

    add code and pictures
    Co-Authored-by: Seth West <seth.west7@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,50 @@
-input {
-  max-width: 30%;
-}
+$(document).ready(function() {
+  $("#formInformation").submit(function(event) {
+    event.preventDefault();
+    $("#marioDiv").hide();
+    $("#dkDiv").hide();
+    $("#masterChief").hide();
+    $("#linkZelda").hide();
+    $("#dixieKong").hide();
+    $("#princessPeach").hide();
+    $("#jigglyPuff").hide();
+    $("#mistyWilliams").hide();
+
+    const genderType = $("select#genderType").val();
+    const facialHair = $("select#facialHair").val();
+    const bodyType = $ ("select#bodyType").val();
+
+    if (genderType === 'male' && facialHair === 'moreHairy' && bodyType === 'smaller') {
+      $("#marioDiv").toggle();
+    }
+
+    if (genderType === 'male' && facialHair === 'moreHairy' && bodyType === 'larger') {
+      $("#dkDiv").toggle();
+    }
+
+    if (genderType === 'male' && facialHair === 'lessHairy' && bodyType === 'larger') {
+      $("#masterChief").toggle();
+    }
+
+    if (genderType === 'male' && facialHair === 'lessHairy' && bodyType === 'smaller') {
+      $("#linkZelda").toggle();
+    }
+
+    if (genderType === 'female' && facialHair === 'moreHairy' && bodyType === 'larger') {
+      $("#dixieKong").toggle();
+    }
+
+    if (genderType === 'female' && facialHair === 'moreHairy' && bodyType === 'smaller') {
+      $("#jigglyPuff").toggle();
+    }
+
+    if (genderType === 'female' && facialHair === 'lessHairy' && bodyType === 'larger') {
+      $("#mistyWilliams").toggle();
+    }
+
+    if (genderType === 'female' && facialHair === 'lessHairy' && bodyType === 'smaller') {
+      $("#princessPeach").toggle();
+    }
+
+  });
+});
